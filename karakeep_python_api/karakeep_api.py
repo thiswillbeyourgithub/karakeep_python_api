45d5135fc9ad3840d460132edae51d177e8314e7
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-05-05 17:30:05 +0200

    bump version 0.1.0 -> 0.1.1

--- karakeep_python_api/karakeep_api.py
+++ karakeep_python_api/karakeep_api.py
@@ -85,7 +85,7 @@ class KarakeepAPI:
     """
 
     # Version reflects the client library version, updated by bumpver
-    VERSION: str = "0.1.0"
+    VERSION: str = "0.1.1"
 
     def __init__(
         self,
