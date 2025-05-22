e73cd62c295e39328b0bd4db2cba6eab7bd62477
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-05-22 21:17:36 +0200

    remove a breakpoint
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- examples/omnivore_highlights_importer/omnivore_highlights_importer.py
+++ examples/omnivore_highlights_importer/omnivore_highlights_importer.py
@@ -193,7 +193,6 @@ def main(
                 r = ratio(omnivore["title"].lower(), content.title.lower())
                 if r >= threshold:
                     found_bm = True
-                    breakpoint()
                     break
 
             if (
