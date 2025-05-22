72d4fe45f311c456390bf72546a7b1b40cf1ca3a
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-05-22 18:37:28 +0200

    bump version 0.1.6 -> 0.1.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="karakeep_python_api",
-    version="0.1.6",
+    version="0.1.7",
     description="Community python client for the Karakeep API.",  # Simplified description
     long_description=long_description,
     long_description_content_type="text/markdown",
