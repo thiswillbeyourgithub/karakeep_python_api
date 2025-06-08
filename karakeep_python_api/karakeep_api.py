34d7eaaa1784691405dcc6aaebd3b445be49b486
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-06-08 20:25:37 +0200

    bump version 1.1.0 -> 1.2.0

--- karakeep_python_api/karakeep_api.py
+++ karakeep_python_api/karakeep_api.py
@@ -85,7 +85,7 @@ class KarakeepAPI:
     """
 
     # Version reflects the client library version, updated by bumpver
-    VERSION: str = "1.1.0"
+    VERSION: str = "1.2.0"
 
     def __init__(
         self,
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="karakeep_python_api",
-    version="1.1.0",
+    version="1.2.0",
     description="Community python client for the Karakeep API.",  # Simplified description
     long_description=long_description,
     long_description_content_type="text/markdown",
